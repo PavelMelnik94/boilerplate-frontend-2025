6eca022d9751fa877ed4c6ea0d8fdc98155244ca
Pavel Melnik <recyclesordie94@gmail.com>
2025-02-05 01:38:04 +0300

    refactor main.ts: reorganize imports for better clarity and consistency

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { setupCounter } from './counter.ts'
 import './style.css'
 import typescriptLogo from './typescript.svg'
+
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
