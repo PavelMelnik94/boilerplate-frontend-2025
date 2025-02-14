898edd07e21510c77a06208ff5491e48f48b0504
PavelMelnik <p.melnik@intelico.su>
2025-02-14 13:32:10 +0300

    chore: update ESLint configuration in VSCode settings, add chain utility exports, and remove typedoc configuration

---
+++ src/utils/functions/DOM/chain/index.ts
@@ -0,0 +1,6 @@
+import { chain } from './chain';
+
+import type { ChainMethods, ExtendedKeyframeAnimationOptions } from './chain';
+
+export { chain };
+export type { ChainMethods, ExtendedKeyframeAnimationOptions };
